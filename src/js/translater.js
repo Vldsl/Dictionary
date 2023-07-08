fee370c65c8c5348f1231023546c152daacf92b7
Vlad Polyakov <VladPolyakov@email.ua>
2023-07-08 18:56:08 +0300

    Added event listeners for toggle select all button, mark undone button, delete all tasks button, delete done tasks button, page navigation, page selection, and add example button. Updated import statements for list actions and variables in vars module.

--- src/js/translater.js
+++ src/js/translater.js
@@ -1,11 +1,57 @@
-import "./../scss/style.scss";
 import { load } from "./modules/utils";
 import { handleAction } from "./modules/handleActions";
-import { searchWords, filterWords, addWord } from "./modules/listActions";
-import { form, list, searchInput, actionFind } from "./modules/vars";
+import {
+  searchWords,
+  filterWords,
+  addWord,
+  toggleSelectAll,
+  resetAllTasks,
+  deleteAllTasks,
+  deleteDoneTasks,
+  handlePageNavigation,
+  handlePageSelection,
+} from "./modules/listActions";
+import {
+  form,
+  list,
+  searchInput,
+  actionFind,
+  toggleSelectAllBtn,
+  markUndoneBtn,
+  delAllWordsBtn,
+  delDoneWordsBtn,
+  words,
+  delWords,
+  pageIndex,
+  numberOfPage,
+  pageControl,
+  addExample,
+  wordInputs,
+} from "./modules/vars";
 
-document.addEventListener("DOMContentLoaded", load);
-form.addEventListener("submit", addWord);
+document.addEventListener("DOMContentLoaded", () => load());
+form.addEventListener("submit", (e) => addWord(e));
 list.addEventListener("click", handleAction);
-searchInput.addEventListener("input", searchWords);
+searchInput.addEventListener("input", () => searchWords(list, searchInput));
 actionFind.addEventListener("input", filterWords);
+
+toggleSelectAllBtn.addEventListener("click", () => toggleSelectAll());
+markUndoneBtn.addEventListener("click", () => resetAllTasks(words));
+delAllWordsBtn.addEventListener("click", () => deleteAllTasks(words));
+delDoneWordsBtn.addEventListener("click", () =>
+  deleteDoneTasks(words, delWords)
+);
+
+pageControl.addEventListener("click", (e) =>
+  handlePageNavigation(e, pageIndex)
+);
+numberOfPage.addEventListener("click", (e) =>
+  handlePageSelection(e, pageIndex)
+);
+
+addExample.addEventListener("click", () => {
+  wordInputs.insertAdjacentHTML(
+    "beforeend",
+    `<input  placeholder='Добавить пример'>`
+  );
+});
